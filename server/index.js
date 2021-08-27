7494733db10b128b66211d5e618c91b5818c6367
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-08-27 10:41:24 +0200

    setup

--- server/index.js
+++ server/index.js
@@ -12,9 +12,13 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Memories API')
+});
+
 app.use('/posts', postRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
