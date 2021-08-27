86f5b2029a3702376616ed849fa6a911672d9535
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-08-27 10:53:12 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+
 app.get('/', (req, res) => {
     res.send('Welcome to Memories API')
 });
-
-app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT;
 
